34c7d548e344b4579e1dddadf58986c24fa3a33a
Jobe Powell <powelljobe@gmail.com>
2021-07-26 13:46:39 -0500

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/jobep/helloworld/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * @author Jobe Powell
+ * email: powelljobe@gmail.com
+ * date: 7/26/2021
+ * purpose: My first Hello World program in an IDE
+ */
+package com.jobep.helloworld;
+
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Jobe!");
+    }
+}
