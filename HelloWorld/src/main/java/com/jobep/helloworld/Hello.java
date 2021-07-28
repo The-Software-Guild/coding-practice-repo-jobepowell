a4c170908ecba702612b32c1e7454d529b30595e
Jobe Powell <powelljobe@gmail.com>
2021-07-28 09:43:16 -0500

    Class Roster Code-Along | Step 3: Menu System

--- HelloWorld/src/main/java/com/jobep/helloworld/Hello.java
+++ HelloWorld/src/main/java/com/jobep/helloworld/Hello.java
@@ -9,6 +9,7 @@ package com.jobep.helloworld;
 
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Hello, Jobe!");
+        System.out.println("Hello, World!");
+        System.out.println("My name is Harry!");
     }
 }
